3154422577cd91642574b405d264bf051010dea5
ShunOmine <shun12baseball@gmail.com>
2019-03-07 18:14:34 +0900

    day5 exercise ~edit birthday fortune telling app

--- app/controllers/fortunetellings_controller.rb
+++ app/controllers/fortunetellings_controller.rb
@@ -4,12 +4,11 @@ class FortunetellingsController < ApplicationController
     @birthday = params[:birthday]
     @birth = "#{@birthday[0, 4]}年#{@birthday[4, 2]}月#{@birthday[6, 2]}日"
 
-    @birth_today = "今日があなたの誕生日です。" if @birthday[4, 4].to_i == Date.today.strftime("%m%d").to_i
+    @birth_today = "今日があなたの誕生日です。" if @birthday[4, 4] == Date.today.strftime("%m%d")
 
     @age = (Date.today.strftime("%Y%m%d").to_i - @birthday.to_i) / 10000
 
-    fortune = ["大吉", "中吉", "吉", "小吉", "凶", "大凶"]
-    @fortune = fortune.sample
+    @fortune = ["大吉", "中吉", "吉", "小吉", "凶", "大凶"].sample
 
     #constellationはcommon.rbに記述
     @constellation = constellation(@birthday[4, 2].to_i, @birthday[6, 2].to_i)
